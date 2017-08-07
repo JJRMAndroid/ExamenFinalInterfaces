e20a5098b142a6561880714ebf5109afb4164e95
pepejjrm <polito.eli@gmail.com>
2017-08-06 23:15:51 -0500

    Cambios 06082017
    Cambios 06082017

--- ExamenFinalInterfaces/app/src/main/java/com/nextu/jj/examenfinalinterfaces/MainActivity.java
+++ ExamenFinalInterfaces/app/src/main/java/com/nextu/jj/examenfinalinterfaces/MainActivity.java
@@ -1,87 +1,121 @@
 package com.nextu.jj.examenfinalinterfaces;
 
-import android.app.Activity;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.support.design.widget.AppBarLayout;
+import android.os.Build;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
-    Toolbar toolbar;
+
+    NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.redes_sociales);
-        toolbar.setSubtitle(R.string.seccion1);
-        setSupportActionBar(toolbar);
-
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, new AppBarWithTabsFragment())
+                .commit();
 
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         if (navigationView != null)
             navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    public void updateView(String title) {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null)
+            toolbar.setTitle(title);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.addDrawerListener(toogle);
+        toogle.syncState();
     }
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         int idItem = item.getItemId();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        int colorPrimary = 0;
+        int colorPrimaryDark = 0;
 
         switch (idItem) {
             case R.id.nav_home:
-                toolbar.setSubtitle(R.string.seccion1);
-                ImageView imageView = (ImageView)findViewById (R.id.img_figura);
-                imageView.setImageResource(R.drawable.home3);
+                colorPrimary = ContextCompat.getColor(this, R.color.colorPrimary);
+                colorPrimaryDark = ContextCompat.getColor(this, R.color.colorPrimaryDark);
+                //toolbar.setSubtitle(R.string.seccion1);
                 break;
             case R.id.nav_facebook:
-                toolbar.setSubtitle(R.string.seccion2);
-                LinearLayout linearLayout = (LinearLayout) findViewById(R.id.nav_header_container);
-                linearLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryFacebook));
-                ImageView imageView2 = (ImageView)findViewById (R.id.img_figura);
-                imageView2.setImageResource(R.drawable.noticias);
+                //toolbar.setSubtitle(R.string.seccion2);
+                colorPrimary = ContextCompat.getColor(this, R.color.colorPrimaryFacebook);
+                colorPrimaryDark = ContextCompat.getColor(this, R.color.colorPrimaryDarkFacebook);
                 break;
             case R.id.nav_instagram:
-                toolbar.setSubtitle(R.string.seccion3);
-                LinearLayout linearLayout1 = (LinearLayout) findViewById(R.id.nav_header_container);
-                linearLayout1.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryInstagram));
-                //imageView2.setImageResource(R.drawable.noticias);
+                //toolbar.setSubtitle(R.string.seccion3);
+                colorPrimary = ContextCompat.getColor(this, R.color.colorPrimaryInstagram);
+                colorPrimaryDark = ContextCompat.getColor(this, R.color.colorPrimaryDarkInstagram);
                 break;
             case R.id.nav_google_plus:
-                toolbar.setSubtitle(R.string.seccion4);
-                LinearLayout linearLayout2 = (LinearLayout) findViewById(R.id.nav_header_container);
-                linearLayout2.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryGooglePlus));
+                //toolbar.setSubtitle(R.string.seccion4);
+                colorPrimary = ContextCompat.getColor(this, R.color.colorPrimaryGooglePlus);
+                colorPrimaryDark = ContextCompat.getColor(this, R.color.colorPrimaryDarkGooglePlus);
                 break;
             case R.id.nav_twiter:
-                toolbar.setSubtitle(R.string.seccion5);
-                LinearLayout linearLayout3 = (LinearLayout) findViewById(R.id.nav_header_container);
-                linearLayout3.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryTwiter));
+                //toolbar.setSubtitle(R.string.seccion5);
+                colorPrimary = ContextCompat.getColor(this, R.color.colorPrimaryTwiter);
+                colorPrimaryDark = ContextCompat.getColor(this, R.color.colorPrimaryDarkTwiter);
                 break;
         }
-
+        cambiaColor(colorPrimary, colorPrimaryDark);
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void cambiaColor(int colorPrimary, int colorPrimaryDark){
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.nav_header_container);
+        linearLayout.setBackgroundColor(colorPrimary);
+
+        Drawable bgColor = new ColorDrawable(colorPrimary);
+
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setBackgroundDrawable(bgColor);
+        }
+
+        if (Build.VERSION.SDK_INT >= 21){
+            getWindow().setStatusBarColor(colorPrimaryDark);
+        }
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setBackgroundColor(colorPrimary);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
     }
 }
---
+++ ExamenFinalInterfaces/app/src/main/java/com/nextu/jj/examenfinalinterfaces/adaptador/BaseViewPagerAdapter.java
@@ -0,0 +1,34 @@
+package com.nextu.jj.examenfinalinterfaces.adaptador;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.nextu.jj.examenfinalinterfaces.BasicFragment;
+import com.nextu.jj.examenfinalinterfaces.R;
+
+public class BaseViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    private String[] tabs;
+
+    public BaseViewPagerAdapter(FragmentManager manager, Context context) {
+        super(manager);
+        tabs = context.getResources().getStringArray(R.array.tabs);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return BasicFragment.getInstance(tabs[position]);
+    }
+
+    @Override
+    public int getCount() {
+        return tabs.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabs[position];
+    }
+}
